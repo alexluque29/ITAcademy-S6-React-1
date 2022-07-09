0ff39f2d042f269c8e823f4e0d4abec7e6fb0f6c
Alex Luque <alexluque29@gmail.com>
2022-07-09 17:36:28 +0200

    Ejercicios 1 y 2

---
+++ src/components/escena/Escena.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Escena(props) {
+    return (
+        <div>
+        <p>{props.texto}</p>
+        </div>
+    )
+}   
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Escena from './components/escena/Escena';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Escena />
   </React.StrictMode>
 );
 
